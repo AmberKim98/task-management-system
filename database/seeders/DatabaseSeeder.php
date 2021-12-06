ef9853cf23a3a736331a0086183cf86c65d901e4
DESKTOP-0SQ8TJS\EaintHmueThapye <scm.eainthmue@gmail.com>
2021-12-06 15:53:20 +0630

    remove foreign key check

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,14 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-
         $this->call([
             EmployeesTableSeeder::class,
             ProjectsTableSeeder::class,
             TasksTableSeeder::class
         ]);
-
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
